fce74d239b0bc733b5c0de4359bd79e18bb3b556
Alex Byrne <c15492952@mydit.ie>
2019-03-26 10:08:07 +0000

    added coroutine and targets

--- Assets/Base.cs
+++ Assets/Base.cs
@@ -11,6 +11,9 @@ public class Base : MonoBehaviour
 
     public GameObject fighterPrefab;
 
+    public GameObject target1;
+    public GameObject target2;
+    public GameObject target3;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +22,27 @@ public class Base : MonoBehaviour
         {
             r.material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
         }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         text.text = "" + tiberium;
+        StartCoroutine("AddTiberium");
+
+        if(tiberium >= 10)
+        {
+            //Instantiate(fighterPrefab, new Vector3(0, 0, 0), );
+        }
+    }
+
+    IEnumerator AddTiberium()
+    {
+        for(;;)
+        {
+            tiberium += 1;
+            yield return new WaitForSeconds(1);
+        }
     }
 }
