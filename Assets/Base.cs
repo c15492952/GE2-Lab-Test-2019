fa0f0e4da10d071c8d3677a9d2f903e8caf96d8d
Alex Byrne <c15492952@mydit.ie>
2019-03-26 11:22:57 +0000

    fixed issues

--- Assets/Base.cs
+++ Assets/Base.cs
@@ -15,25 +15,71 @@ public class Base : MonoBehaviour
     public GameObject target2;
     public GameObject target3;
 
+    private Renderer shiprend;
+    private Component seek;
+    Color teamColor;
+
     // Start is called before the first frame update
     void Start()
     {
+        teamColor = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
         foreach (Renderer r in GetComponentsInChildren<Renderer>())
         {
-            r.material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
+            r.material.color = teamColor;
         }
-        
+        StartCoroutine("AddTiberium");
     }
 
     // Update is called once per frame
     void Update()
     {
         text.text = "" + tiberium;
-        StartCoroutine("AddTiberium");
+        
+       
 
         if(tiberium >= 10)
         {
-            //Instantiate(fighterPrefab, new Vector3(0, 0, 0), );
+            int TargPick = Random.Range(1, 3);
+            if (TargPick == 1)
+            {
+                tiberium -= 10;
+                GameObject ship = Instantiate(fighterPrefab, transform.position, Quaternion.LookRotation(target1.transform.position, Vector3.up));
+                ship.AddComponent<Renderer>();
+                shiprend = ship.GetComponent<Renderer>();
+                shiprend.material.color = teamColor;
+
+                ship.AddComponent<Boid>();
+                ship.AddComponent<Arrive>();
+                ship.AddComponent<Seek>();
+                ship.GetComponent<Seek>().targetGameObject = target1;
+            }
+            else if(TargPick == 2)
+            {
+                tiberium -= 10;
+                GameObject ship = Instantiate(fighterPrefab, transform.position, Quaternion.LookRotation(target1.transform.position, Vector3.up));
+                ship.AddComponent<Renderer>();
+                shiprend = ship.GetComponent<Renderer>();
+                shiprend.material.color = teamColor;
+
+                ship.AddComponent<Boid>();
+                ship.AddComponent<Arrive>();
+                ship.AddComponent<Seek>();
+                ship.GetComponent<Seek>().targetGameObject = target2;
+            }
+            else if(TargPick ==3)
+            {
+                tiberium -= 10;
+                GameObject ship = Instantiate(fighterPrefab, transform.position, Quaternion.LookRotation(target1.transform.position, Vector3.up));
+                ship.AddComponent<Renderer>();
+                shiprend = ship.GetComponent<Renderer>();
+                shiprend.material.color = teamColor;
+
+                ship.AddComponent<Boid>();
+                ship.AddComponent<Arrive>();
+                ship.AddComponent<Seek>();
+                ship.GetComponent<Seek>().targetGameObject = target3;
+            }
+               
         }
     }
 
